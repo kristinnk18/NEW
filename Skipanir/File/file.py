d4d7528178f505534d31c96fd1b102ffed6a407a
Kristinn Örn <kristinnorn@clients-pool-6.nat.ru.is>
2018-09-20 12:40:29 +0000

    a

--- Skipanir/File/file.py
+++ Skipanir/File/file.py
@@ -1,4 +1,10 @@
-file_content = open("test.txt" , "r")
+# file_content = open("test.txt", "r", encoding="utf-8")
 
-for word in file_content:
-    print(word)
+# for word in file_content:
+#     print(word,end="")
+
+# file_content.close()
+
+
+with open("test.txt", "w", encoding="utf-8") as file_content:
+    print("afram njardvik", file=file_content)
